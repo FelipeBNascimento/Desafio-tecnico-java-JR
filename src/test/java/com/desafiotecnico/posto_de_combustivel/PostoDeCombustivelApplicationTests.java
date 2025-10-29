d6d8875cb1780353d57b038690c83cc5a26970c6
Bazan <felipe_bazan3006@hotmail.com>
2025-10-29 17:49:25 -0300

    Commit inicial

---
+++ src/test/java/com/desafiotecnico/posto_de_combustivel/PostoDeCombustivelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiotecnico.posto_de_combustivel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostoDeCombustivelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
